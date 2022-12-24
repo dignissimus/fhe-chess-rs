675b5a6094fbec8e572c7228a8ad427967c3d60d
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-12-24 00:49:24 +0000

    Add board serialisation functionality

---
+++ src/common.rs
@@ -0,0 +1,95 @@
+use chess::{Board, Color, File, Piece, Rank, Square};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub enum ServerState {
+    AwaitingGame,
+    AwaitingMove,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Position {
+    data: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PieceType {
+    role: Role,
+    colour: Colour,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Role {
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Colour {
+    Black,
+    White,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ChessMessage {
+    StreamMove {
+        identifier: u8,
+        movement: Move,
+    },
+    ReadEvaluations, // Remove me, only use the below
+    EvaluationResult {
+        identifier: u8,
+        evaluation: Evaluation,
+    },
+}
+
+type Evaluation = i8;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Move {
+    pub positions: Vec<Position>,
+}
+
+fn bitboard_location(piece: Piece, color: Color) -> usize {
+    let piece_index = match piece {
+        Piece::Pawn => 0,
+        Piece::Knight => 1,
+        Piece::Bishop => 2,
+        Piece::Rook => 3,
+        Piece::Queen => 4,
+        Piece::King => 5,
+    };
+    let colour_index = match color {
+        Color::Black => 0,
+        Color::White => 1,
+    };
+
+    colour_index * (64 * 6) + piece_index * 64
+}
+
+impl Position {
+    pub fn from_board(board: Board) -> Position {
+        let mut serialised = vec![0; 64 * 12 + 1];
+        for rank in 0..7 {
+            for file in 0..7 {
+                let rank = Rank::from_index(rank);
+                let file = File::from_index(file);
+                let square = Square::make_square(rank, file);
+                if let Some(piece) = board.piece_on(square) {
+                    let color = board.color_on(square).unwrap();
+                    let location = bitboard_location(piece, color);
+                    serialised[location] = 1;
+                }
+            }
+        }
+        serialised[64 * 12] = match board.side_to_move() {
+            Color::Black => 0,
+            Color::White => 1,
+        };
+        Position { data: serialised }
+    }
+}
--- src/server/main.rs
+++ src/server/main.rs
@@ -1,66 +1,9 @@
-use concrete::prelude::*;
-use concrete::{generate_keys, set_server_key, ConfigBuilder, FheBool};
-use serde::{Deserialize, Serialize};
-use serde_json::{Map, Result, Value};
-use std::net::TcpListener;
-use std::sync::{Arc, Mutex, mpsc};
-use std::thread;
-use tungstenite;
-use tungstenite::Message;
-use tungstenite::Message::*;
+use fhe_chess_rs::common::*;
 use std::fs;
-
-const SIZE: usize = 561;
-const N_THREADS: u8 = 8;
-
-#[derive(Serialize, Deserialize)]
-enum ServerState {
-    AwaitingGame,
-    AwaitingMove,
-}
-
-type Position = Bitboard;
-
-#[derive(Serialize, Deserialize)]
-struct PieceType {
-    role: Role,
-    colour: Colour,
-}
-
-#[derive(Serialize, Deserialize)]
-enum Role {
-    Pawn,
-    Knight,
-    Bishop,
-    Rook,
-    Queen,
-    King,
-}
-
-#[derive(Serialize, Deserialize)]
-enum Colour {
-    Black,
-    White,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Bitboard {
-    data: Vec<bool>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum ChessMessage {
-    StreamMove { identifier: u8, movement: Move },
-    ReadEvaluations, // Remove me, only use the below
-    EvaluationResult { identifier: u8, evaluation: Evaluation }
-}
-
-type Evaluation = i8;
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Move {
-    positions: Vec<Position>,
-}
+use std::net::TcpListener;
+use std::sync::mpsc;
+use std::thread;
+use tungstenite::Message::*;
 
 fn main() {
     println!(
@@ -78,14 +21,14 @@ fn main() {
     );
 
     let weights = fs::read_to_string("weights.json").unwrap();
-    let weights: Vec<i8> = serde_json::from_str(&weights).unwrap();
+    let _weights: Vec<i8> = serde_json::from_str(&weights).unwrap();
     let server = TcpListener::bind("0.0.0.0:8085").unwrap();
 
     for stream in server.incoming() {
         let stream = stream.unwrap();
         thread::spawn(move || {
             let websocket = tungstenite::accept(stream);
-            let (transmitter, receiver) = mpsc::channel::<ChessMessage>();
+            let (_transmitter, receiver) = mpsc::channel::<ChessMessage>();
             match websocket {
                 Err(msg) => {
                     println!("Error while accepting websocket connection: {}", msg);
@@ -94,7 +37,7 @@ fn main() {
                 Ok(mut websocket) => loop {
                     let message = websocket.read_message().unwrap();
                     if let Text(json) = message {
-                        let data: Result<ChessMessage> = serde_json::from_str(&json);
+                        let data = serde_json::from_str(&json);
                         if let Err(msg) = data {
                             println!("Malformed data: {}", msg);
                             continue;
@@ -105,11 +48,15 @@ fn main() {
                             ChessMessage::StreamMove { .. } => {
                                 let evaluations: Vec<ChessMessage> = receiver.try_iter().collect();
                                 let response = serde_json::to_string(&evaluations).unwrap();
-                                websocket.write_message(Text(response));
+                                websocket
+                                    .write_message(Text(response))
+                                    .expect("Error sending the message");
                             }
                             ChessMessage::ReadEvaluations => {
                                 let response = serde_json::to_string(&vec![1, 2, 3]).unwrap();
-                                websocket.write_message(Text(response));
+                                websocket
+                                    .write_message(Text(response))
+                                    .expect("Error sending the message");
                             }
 
                             _ => {}
@@ -118,53 +65,5 @@ fn main() {
                 },
             }
         });
-    }
-}
-
-fn test_main() {
-    let config = ConfigBuilder::all_disabled().enable_default_bool().build();
-    let (client_key, server_key) = generate_keys(config);
-    let queue: Arc<Mutex<Vec<(FheBool, FheBool)>>> = Arc::new(Mutex::new(Vec::new()));
-
-    for _ in 1..SIZE {
-        let a = FheBool::encrypt(false, &client_key);
-        let b = FheBool::encrypt(true, &client_key);
-        let mut queue = queue.lock().unwrap();
-        queue.push((a, b));
-        drop(queue);
-    }
-
-    let mut threads: Vec<thread::JoinHandle<()>> = Vec::new();
-    for _ in 1..N_THREADS {
-        let queue = queue.clone();
-        let key = server_key.clone();
-        threads.push(thread::spawn(move || {
-            set_server_key(key);
-            loop {
-                let mut queue = queue.lock().unwrap();
-                let data = queue.pop();
-                drop(queue);
-                if let Some((left, right)) = data {
-                    let _result = left & right;
-                } else {
-                    break;
-                }
-            }
-        }));
-    }
-
-    let mut psize = 0;
-    loop {
-        let queue = queue.clone();
-        let length = queue.lock().unwrap().len();
-        drop(queue);
-        if length == 0 {
-            break;
-        }
-        if length != psize {
-            println!("Queue size: {}", length);
-            psize = length;
-        }
-        thread::sleep(std::time::Duration::from_secs(1));
     }
 }
