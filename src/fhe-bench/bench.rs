f0735af6af11ade91633e107dc75e8a46f5e96f5
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-12-24 20:34:18 +0000

    Create key generation binary
    Signed-off-by: Sam Ezeh <sam.z.ezeh@gmail.com>

--- src/fhe-bench/bench.rs
+++ src/fhe-bench/bench.rs
@@ -1,15 +1,14 @@
 use concrete::prelude::*;
 use concrete::{generate_keys, set_server_key, ConfigBuilder, FheUint8};
-use std::time::{Instant, Duration};
+use std::time::{Duration, Instant};
 
 fn bench(a: &FheUint8, b: &FheUint8) {
     let result = a + b;
 }
 
-
 fn main() {
     println!("Generating keys...");
-    let start = Duration::Instant::now();
+    let start = Instant::now();
     let config = ConfigBuilder::all_disabled().enable_default_uint8().build();
     let (client_key, server_key) = generate_keys(config);
     println!("Took {:?} to generate server keys", start.elapsed());
@@ -26,6 +25,8 @@ fn main() {
     for _ in 1..=10 {
         let _result = &a + &b;
     }
-    println!("Took {:?} to perform 10 additions on 8-bit integers", start.elapsed());
+    println!(
+        "Took {:?} to perform 10 additions on 8-bit integers",
+        start.elapsed()
+    );
 }
-
---
+++ src/generate-keys/main.rs
@@ -0,0 +1,21 @@
+use bincode;
+use concrete::{generate_keys, ConfigBuilder};
+use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let config = ConfigBuilder::all_disabled()
+        .enable_default_uint12()
+        .build();
+    println!("Generating encryption keys...");
+    let (client_key, server_key) = generate_keys(config);
+
+    println!("Writing encryption keys to disk");
+    let mut file = File::create("client-key.bin").unwrap();
+    file.write_all(&bincode::serialize(&client_key).unwrap())
+        .expect("Unable to write client key to file");
+
+    let mut file = File::create("server-key.bin").unwrap();
+    file.write_all(&bincode::serialize(&server_key).unwrap())
+        .expect("Unable to write server key to file");
+}
